82f793ab9c39fcf64193dd1a8a5ba8988d3b65ac
juliette.lofaro@gmail.com <juliette.lofaro@gmail.com>
2018-03-21 21:18:30 +0100

    Modify booking entity

--- src/OC/ShopBundle/Entity/Booking.php
+++ src/OC/ShopBundle/Entity/Booking.php
@@ -322,23 +322,5 @@ class Booking //implements ArrayAccess
         $this->prixTotal = $prixTotal;
     }
 
-    public function offsetSet($offset, $value) {
-        if (is_null($offset)) {
-            $this->container[] = $value;
-        } else {
-            $this->container[$offset] = $value;
-        }
-    }
 
-    public function offsetExists($offset) {
-        return isset($this->container[$offset]);
-    }
-
-    public function offsetUnset($offset) {
-        unset($this->container[$offset]);
-    }
-
-    public function offsetGet($offset) {
-        return isset($this->container[$offset]) ? $this->container[$offset] : null;
-    }
 }
