66c652fc637b68b7f92c3d6a6d909c1a9789ca8c
juliette.lofaro@gmail.com <juliette.lofaro@gmail.com>
2018-03-27 17:43:00 +0200

    OutilPaymentTest terminé

--- tests/Controller/OutilPaymentTest.php
+++ tests/Controller/OutilPaymentTest.php
@@ -11,8 +11,8 @@ class OutilPaymentTest extends TestCase
 {
     public function testcalculPrixCommande()
     {
-        //creation d'un tiket
-        //
+        //GRATUIT
+
         $booking = new Booking();
         $ticket = new Ticket();
 
@@ -25,16 +25,55 @@ class OutilPaymentTest extends TestCase
         $dateDeVisite = new DateTime('06/06/2018');
 
 
+
         $booking->setDatedevisite($dateDeVisite);
         $ticket->setBooking($booking);
 
         $outilPayment = new OutilPayment();
         $prixTotal = $outilPayment->calculPrixCommande($booking);
 
-
         $this->assertSame(0, $prixTotal);
+        $booking->removeTicket($ticket);
 
 
+        $dateDeNaissance = new DateTime('02/31/1995');
+        $ticket->setDatedenaissance($dateDeNaissance);
+        $booking->addTicket($ticket);
+        $prixTotal = $outilPayment->calculPrixCommande($booking);
+        $this->assertSame(16, $prixTotal);
+        $booking->removeTicket($ticket);
 
+
+        //TARIF ENFANT
+        $dateDeNaissance = new DateTime('02/31/2009');
+        $ticket->setDatedenaissance($dateDeNaissance);
+        $booking->addTicket($ticket);
+        $prixTotal = $outilPayment->calculPrixCommande($booking);
+        $this->assertSame(8, $prixTotal);
+        $booking->removeTicket($ticket);
+
+        //TARIF NORMAL
+        $dateDeNaissance = new DateTime('02/31/1995');
+        $ticket->setDatedenaissance($dateDeNaissance);
+        $booking->addTicket($ticket);
+        $prixTotal = $outilPayment->calculPrixCommande($booking);
+        $this->assertSame(16, $prixTotal);
+        $booking->removeTicket($ticket);
+
+        //TARIF SENIOR
+        $dateDeNaissance = new DateTime('02/31/1950');
+        $ticket->setDatedenaissance($dateDeNaissance);
+        $booking->addTicket($ticket);
+        $prixTotal = $outilPayment->calculPrixCommande($booking);
+        $this->assertSame(12, $prixTotal);
+        $booking->removeTicket($ticket);
+
+        //TARIF REDUIT
+        $reduit = true;
+        $ticket->setReduit($reduit);
+        $booking->addTicket($ticket);
+        $prixTotal = $outilPayment->calculPrixCommande($booking);
+        $this->assertSame(10, $prixTotal);
+        $booking->removeTicket($ticket);
     }
 }
